54347cc9739a6a2e3ac5cc52346438e43552df91
Dan-Santarossa <dpsantarossa@gmail.com>
2022-07-12 17:40:22 -0400

    PIL extract

--- S3/s3_resize_lambda_func.py
+++ S3/s3_resize_lambda_func.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 
 import boto3
-from PIL import Image
+from PIL import Image #source in AWS
 
 s3 = boto3.client('s3')
 DEST_BUCKET = os.environ['DEST_BUCKET']
@@ -12,19 +12,19 @@ SIZE = 128, 128
 def lambda_handler(event, context):
 
     for record in event['Records']:
-        source_bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
-        thumb = 'thumb-' + key
+        source_bucket = record['s3']['bucket']['name'] #
+        key = record['s3']['object']['key'] #object that was uploaded
+        thumb = 'thumb-' + key #renames file with prefix thumb-
         with tempfile.TemporaryDirectory() as tmpdir:
-            download_path = os.path.join(tmpdir, key)
-            upload_path = os.path.join(tmpdir, thumb)
+            download_path = os.path.join(tmpdir, key) #specify download path
+            upload_path = os.path.join(tmpdir, thumb) 
             s3.download_file(source_bucket, key, download_path)
             generate_thumbnail(download_path, upload_path)
             s3.upload_file(upload_path, DEST_BUCKET, thumb)
 
-        print('Thumbnail image saved at {}/{}'.format(DEST_BUCKET, thumb))
+        print('Thumbnail image saved at {}/{}'.format(DEST_BUCKET, thumb))#print to cloudwatch logs
 
-
+#resize image
 def generate_thumbnail(source_path, dest_path):
     print('Generating thumbnail from:', source_path)
     with Image.open(source_path) as image:
